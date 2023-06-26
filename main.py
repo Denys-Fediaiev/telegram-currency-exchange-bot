10459c9f99b97b72b157b1baec0a067cb3cd0244
raydavis <wingeeddd2gmail.com>
2023-06-26 13:20:05 +0100

    added error handling

--- main.py
+++ main.py
@@ -4,30 +4,7 @@ import requests
 import logging
 from requests.structures import CaseInsensitiveDict
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-API_KEY = "#"
-
-bot = telebot.TeleBot("#")
-
-
-logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
-                    format="%(asctime)s %(levelname)s %(message)s")
-
-
-@bot.message_handler(commands=['start', 'hello'])
-def send_welcome(message):
-    bot.reply_to(message, "Hello! I'm Bot That will help you to convert currencies!\n If you want to start type /exchange")
-
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-# @bot.message_handler(func=lambda message: True)
-# def echo_message(message):
-#     bot.reply_to(message, message.text)
-
-@bot.message_handler(commands=['help', 'list'])
-def currency_list(message):
-    bot.reply_to(message, "Here's currency codes supported by us:\n" 
-    """
+currency = """
     EUR
     USD
     JPY
@@ -60,7 +37,31 @@ def currency_list(message):
     PHP
     SGD
     THB
-    ZAR """)
+    ZAR """
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+API_KEY = "#"
+
+bot = telebot.TeleBot("#")
+
+logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
+                    format="%(asctime)s %(levelname)s %(message)s")
+
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message,
+                 "Hello! I'm Bot That will help you to convert currencies!\n If you want to start,  type /exchange")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+# @bot.message_handler(func=lambda message: True)
+# def echo_message(message):
+#     bot.reply_to(message, message.text)
+
+@bot.message_handler(commands=['help', 'list'])
+def currency_list(message):
+    bot.reply_to(message, f"Here's currency codes supported by us:\n {currency}")
 
 
 def currency_exchanger(currency_in: str, currency_out: str):
@@ -81,43 +82,71 @@ def currency_in_handler(message):
 
 def currency_out_handler(message):
     currency_in = message.text
-    text = "what currency do you want to convert to?"
-    sent_msg = bot.send_message(
-        message.chat.id, text, parse_mode="Markdown")
-    bot.register_next_step_handler(
-        sent_msg, amount_handler, currency_in.upper())
+    try:
+        if not currency_in.upper() in currency:
+            raise Exception("Error")
+        logging.info(f"user chose {currency_in} currency")
+
+    except:
+        logging.error(f"ERROR: Wrong currency selected: {currency_in}")
+        sent_msg = bot.send_message(
+            message.chat.id, f"Wrong currency selected: {currency_in} \n If you want to start again, type /start", parse_mode="Markdown")
+        bot.register_next_step_handler(
+            sent_msg)
+    else:
+        text = "what currency do you want to convert to?"
+        sent_msg = bot.send_message(
+            message.chat.id, text, parse_mode="Markdown")
+        bot.register_next_step_handler(
+            sent_msg, amount_handler, currency_in.upper())
 
 
 def amount_handler(message, currency_in):
     currency_out = message.text
-    text = f"what amount of {currency_in} do you want to convert to {currency_out}?"
-    sent_msg = bot.send_message(
-        message.chat.id, text, parse_mode="Markdown")
-    bot.register_next_step_handler(
-        sent_msg, currency_fetch, currency_in.upper(), currency_out.upper())
+    try:
+        if not currency_out.upper() in currency:
+            raise Exception("Error")
+        logging.info(f"user chose {currency_out} currency")
+
+    except:
+        logging.error(f"ERROR: Wrong currency selected: {currency_out}")
+        sent_msg = bot.send_message(
+            message.chat.id, f"Wrong currency selected: {currency_out} \n If you want to start again, type /start", parse_mode="Markdown")
+        bot.register_next_step_handler(
+            sent_msg)
+    else:
+        text = f"what amount of {currency_in} do you want to convert to {currency_out}?"
+        sent_msg = bot.send_message(
+            message.chat.id, text, parse_mode="Markdown")
+        bot.register_next_step_handler(
+            sent_msg, currency_fetch, currency_in.upper(), currency_out.upper())
 
 
 def currency_fetch(message, currency_in, currency_out):
-    amount = float(message.text)
-    exchange_c = currency_exchanger(currency_in, currency_out)
-    print(exchange_c)
-    logging.info(f"json format: {exchange_c}")
-    logging.info(f"currency code IN: {currency_in}")
-    logging.info(f"currency code OUT: {currency_out}")
-    logging.info(f"amount: {amount}")
     try:
-        data = exchange_c['data']
-        logging.info(f"fetching successful with data result:{data}")
+        amount = float(message.text)
     except:
-        logging.error(f"Error: {data}", exc_info=True)
-    result = data[currency_out] * amount
-    exchange_message = f'Current {currency_in} to {currency_out} rate is  {data[currency_out]}\n*your currency:* {currency_in} {amount}\n*you get:* {currency_out} {result}'
-    bot.send_message(message.chat.id, "Here's your info!")
-    bot.send_message(message.chat.id, exchange_message, parse_mode="Markdown")
-
-
-
+        logging.error(f"ERROR: amount should be a number, not: {message.text}")
+        sent_msg = bot.send_message(
+            message.chat.id, f"ERROR: amount should be a number, not: {message.text} \n If you want to start again, type /start", parse_mode="Markdown")
+        bot.register_next_step_handler(
+            sent_msg)
+    else:
+        exchange_c = currency_exchanger(currency_in, currency_out)
+        print(exchange_c)
+        logging.info(f"json format: {exchange_c}")
+        # logging.info(f"currency code IN: {currency_in}")
+        # logging.info(f"currency code OUT: {currency_out}")
+        logging.info(f"amount: {amount}")
+        try:
+            data = exchange_c['data']
+            logging.info(f"fetching successful with data result:{data}")
+        except:
+            logging.error(f"Error: {data}", exc_info=True)
+        result = data[currency_out] * amount
+        exchange_message = f'Current {currency_in} to {currency_out} rate is  {data[currency_out]}\n*your currency:* {currency_in} {amount}\n*you get:* {currency_out} {result}'
+        bot.send_message(message.chat.id, "Here's your info!")
+        bot.send_message(message.chat.id, exchange_message, parse_mode="Markdown")
 
 
 bot.infinity_polling()
-
